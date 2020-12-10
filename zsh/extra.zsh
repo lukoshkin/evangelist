48ca134ccab1835d3e0c5a6cd3568ce72835964e
lukoshkin <lukoshkin@phystech.edu>
2020-12-10 23:40:32 +0300

    Fix color scheme

--- zsh/extra.zsh
+++ zsh/extra.zsh
@@ -110,17 +110,10 @@ bindkey '^[w' forward-word  # complete word in a suggestion
 ## * To find some laptop (Ubuntu) bindings that contain <pattern>,
 ##   use 'gsettings list-recursively | grep <pattern>'.
 
-ZLS_COLORS="no=00:fi=00:di=01;34:ln=01;36:\ 
-  pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:\ 
-  or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:\ 
-  *.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:\ 
-  *.z=01;31:*.Z=01;31:*.gz=01;31:*.deb=01;31:\ 
-  *.jpg=01;35:*.gif=01;35:*.bmp=01;35:*.ppm=01;35:\ 
-  *.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:\ 
-  *.mpg=01;37:*.avi=01;37:*.gl=01;37:*.dl=01;37:"
-
+## Export standard ls colors (grep selects everything between '')
+export LS_COLORS=$(dircolors | grep -Po "'\K.*(?=')")
 ## Sets for the completion menu "similar to ls command" colors.
-zstyle ':completion:*' list-colors ${(s.:.)ZLS_COLORS}
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 ## 'ignorecase'+'smartcase'+'hyphen_insensitive' completion on the cmd line.
 zstyle ':completion:*' matcher-list 'm:{[:lower:]-_}={[:upper:]_-}'
 ##  Highlight for the selected completion menu item.
