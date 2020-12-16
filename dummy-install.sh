00bf4caa65a10c45ccdc63f134d140192ab479da
lukoshkin <lukoshkin@phystech.edu>
2020-12-16 15:56:54 +0300

    Update installation script

--- dummy-install.sh
+++ dummy-install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-# WARNING: This is experimental set-up
+# WARNING: This is experimental set-up. Some lines may come and go.
 
-if [[ -z $LUKOVNADOTFILES ]]; then
-  cat bash/bashrc >> ~/.bashrc
-  export LUKOVNADOTFILES="I am bash-vimmer!"
-  [[ -z $XDG_CONFIG_HOME ]] && export XDG_CONFIG_HOME="$HOME/.config"
-  [[ -z $XDG_DATA_HOME ]] && export XDG_DATA_HOME="$HOME/.local/share"
-  [[ -z $XDG_CACHE_HOME ]] && export XDG_CACHE_HOME="$HOME/.cache"
-fi
+[[ -z $XDG_CONFIG_HOME ]] && export XDG_CONFIG_HOME="$HOME/.config"
+[[ -z $XDG_DATA_HOME ]] && export XDG_DATA_HOME="$HOME/.local/share"
+[[ -z $XDG_CACHE_HOME ]] && export XDG_CACHE_HOME="$HOME/.cache"
 
+# As you can see, the first 3 copies do not follow XDG concept.
+# It might be fixed in further updates.
+cp bash/bashrc ~/.bashrc
 cp tmux.conf ~/.tmux.conf
 cp bash/inputrc ~/.inputrc
 cp -r nvim $XDG_CONFIG_HOME
+
+[[ -n $(which conda) ]] && conda init bash
 
 vimPlug="$XDG_DATA_HOME/nvim/site/autoload/plug.vim"
 if [[ -f $vimPlug ]]; then
@@ -23,16 +24,21 @@ vim +source $XDG_CONFIG_HOME/nvim/init.vim +q
 vim +PlugInstall +qall
 
 
+# PIP_FLAGS=(--no-cache-dir --upgrade)
+# [[ -n $(which conda) ]] && {conda init bash; PIP_FLAGS+=(--user)
+
+PIP_FLAGS=(--user --no-cache-dir --upgrade)
+
 if [[ -z $JUPYTER_CONFIG_DIR ]]; then
   if [[ -d ~/.jupyter ]]
   then
     JUPYTER_CONFIG_DIR=~/.jupyter
   else
-    pip install --no-cache-dir --prefix $XDG_CONFIG_HOME --upgrade jupyter
+    pip install ${PIP_FLAGS[@]} --prefix $XDG_CONFIG_HOME jupyter
     JUPYTER_CONFIG_DIR=$XDG_CONFIG_HOME/jupyter
 fi
 
-pip install --no-cache-dir \
+pip install ${PIP_FLAGS[@]} \
   jupyter_contrib_nbextensions \
   jupyter_nbextensions_configurator
 
