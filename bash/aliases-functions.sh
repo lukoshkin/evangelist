1d8ec2ba51df0d5ee896a27e535ce4a03b531478
lukoshkin <lukoshkin@phystech.edu>
2021-02-12 19:45:26 +0300

    Move common parts to aliases-functions.sh
    Make transparency control to work in any mode
    Fix navigation with gg function
    Add autocd to bash settings

---
+++ bash/aliases-functions.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+alias l='ls -lAh'
+alias ll='ls -lAht'
+alias md='mkdir -p'
+
+alias _vimrc="vim $MYVIMRC"
+alias vimrc="vim $XDG_CONFIG_HOME/nvim/custom.vim"
+
+## Folder stack navigation
+alias d='dirs -v'
+alias G='gg 0'
+
+gg () {
+  if [[ -z $1 ]]
+  then
+    pushd +1 &> /dev/null
+    [[ $? -ne 0 ]] && echo Singular dir stack || :
+  elif [[ $1 == '0' ]]
+  then
+    pushd -0 > /dev/null
+  elif [[ $1 =~ ^[0-9]+ ]]
+  then
+    pushd +$1 > /dev/null
+  elif [[ $1 =~ ^-[0-9]+ ]]
+  then
+    popd +${1:1}
+  else
+    echo Wrong args
+  fi
+}
+
+
+## Open the last file closed
+# alias v="vim +'e #<1'"
+alias v="vim +'execute \"normal \<C-P>\<Enter>\"'"
+alias vv="vim +'browse filter !/__\|NERD_tree/ oldfiles'"
+
+alias fd='find . -type d -name'
+alias ff='find . -type f -name'
+alias grep='grep --color'
+
+tree () {
+  local w8
+  local hierarchy
+
+  [[ -n $1 ]] && w8=$1 || w8=.1
+  # 'script' preserves output colors (one of its assets)
+  #  Since script saves the output to a file, /dev/null is used to discard it
+
+  # -e - return exit code of the child process
+  # -q - don't write start-end timestamps
+  # -c - command to execute
+  hierarchy=$(script -eqc "timeout --preserve-status $w8 tree" /dev/null)
+
+  # 143 - SIGTERM (process was killed)
+  if [[ $? -eq 143 ]]
+  then
+    echo 'Try to run it in one of subfolders.'
+    return
+  fi
+
+  # double quotes are required in bash
+  echo "$hierarchy"
+}
--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -1,71 +1,13 @@
-# ALIASES
+# ZSH ALIASES
 # -------
 alias -g ...=../..
 alias -g ....=../../..
 alias -g .....=../../../..
 alias -g ......=../../../../..
-
-alias l='ls -lAh'
-alias ll='ls -lAht'
 alias ls='ls -G'
-alias md='mkdir -p'
 
-## Folder stack navigation
-alias d='dirs -v'
-function gg() {
-  if [[ -z $1 ]]
-  then
-    pushd +1 2> /dev/null
-    [[ $? -ne 0 ]] && echo Singular dir stack
-  elif [[ $1 == '0' ]]
-  then
-    pushd -0
-  elif [[ $1 =~ ^[0-9]+ ]]
-  then
-    pushd +$1
-  elif [[ $1 =~ ^-[0-9]+ ]]
-  then
-    popd $1
-  else
-    echo Wrong args
-  fi
-}
-alias G='gg 0'
-
-## Open the last file closed
-# alias v="vim +'e #<1'"
-alias v="vim +'execute \"normal \<C-P>\<Enter>\"'"
-alias vv="vim +'browse filter !/__\|NERD_tree/ oldfiles'"
-
-alias fd='find . -type d -name'
-alias ff='find . -type f -name'
-alias grep='grep --color'
-
-function tree() {
-  local w8
-  local hierarchy
-
-  [[ -n $1 ]] && w8=$1 || w8=.1
-  # 'script' preserves output colors (one of its assets)
-  #  Since script saves the output to a file, /dev/null is used to discard it
-
-  # -e - return exit code of the child process
-  # -q - don't write start-end timestamps
-  # -c - command to execute
-  hierarchy=$(script -eqc "timeout --preserve-status $w8 tree" /dev/null)
-
-  # 143 - SIGTERM (process was killed)
-  if [[ $? -eq 143 ]]
-  then
-    echo 'Try to run it in one of subfolders.'
-    return
-  fi
-
-  echo $hierarchy
-}
-
-alias zshrc="vim $ZDOTDIR/.zshrc"
-alias vimrc="vim ~/.config/nvim/init.vim"
+alias _zshrc="vim $ZDOTDIR/.zshrc"
+alias zshrc="vim $ZDOTDIR/custom.zsh"
 ## To list all active aliases, run `alias`
 
 
