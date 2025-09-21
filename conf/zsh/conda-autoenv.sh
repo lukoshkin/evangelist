3d4d0f5d9c0526dfaa54a4772ff8c8bb86f139d5
lukoshkin <lukoshkin.workspace@gmail.com>
2025-09-21 13:22:55 +0300

    Refactor conda-autoenv, prepare for venv-extension
    Conda Autoenv Core:
    * Fix array length checking
    c Improve variable naming and add local scope declarations for better encapsulation
    c Add comprehensive variable quoting throughout for safer string handling
    
    Shell Compatibility:
    *+ Replace HISTFILE-based shell detection with ZSH_VERSION and BASH_VERSION checks
    *+ Add KSH_ARRAYS option support for zsh array indexing compatibility
     + Implement proper error handling for unsupported shells

--- conf/zsh/conda-autoenv.sh
+++ conf/zsh/conda-autoenv.sh
@@ -1,73 +1,77 @@
 #!/bin/bash
 
 up_hierarchy_search() {
-  if [[ -z $1 || -z $2 ]]
-  then
+  if [[ -z $1 || -z $2 ]]; then
     echo up_hierarchy_search: missing args.
     echo 'Try: up_hierarchy_search <dir> <file>`'
     return 1
   fi
 
-  local found max_recursion_depth=100
-  ## max_recursion_depth is for safety measures
+  ## `max_recursion_depth` to avoid infinite recursion
+  local max_recursion_depth=100
 
-  _up_hierarchy_search () {
+  _up_hierarchy_search() {
     [[ $1 = / ]] && return 1
     ## 'ls $1/$2' is simpler but doesn't fit zsh
     ## (by default, zsh prints an error msg to stdout on a failed globbing).
-    found=$(find "$1" -maxdepth 1 -type f -name "$2")
+    local found_evnfiles
+    found_evnfiles=$(find "$1" -maxdepth 1 -type f -name "$2")
 
-    if [[ -z $found ]] && [[ $max_recursion_depth -gt 0 ]]
-    then
-      (( max_recursion_depth-- ))
-      _up_hierarchy_search "$(dirname "$1")" "$2"
+    if [[ -n $found_evnfiles ]] ||
+      [[ $((max_recursion_depth--)) -le 0 ]]; then
+      echo "$found_evnfiles"
+      return
     fi
+
+    _up_hierarchy_search "$(dirname "$1")" "$2"
   }
 
   local parent
-  parent=$(realpath "$1" 2> /dev/null) \
-    || parent=$(readlink -f "$1" 2> /dev/null) || return
+  parent=$(realpath "$1" 2>/dev/null) ||
+    parent=$(readlink -f "$1" 2>/dev/null) || return
 
   _up_hierarchy_search "$parent" "$2"
-  echo "$found"
 }
 
-
-_conda_autoenv () {
+_conda_autoenv() {
   ## Disable conda-autoenv by setting AUENV_SHELL to false.
   ## Disabling functionality locally (or temporarily) may be useful
   ## when there is another plugin that temporarily takes care of managing
   ## conda environments.
   [[ $AUENV_SHELL == false ]] && return
 
-  [[ -z $1 ]] && { echo Not enough args provided; return 1; }
+  [[ -z $1 ]] && {
+    echo Not enough args provided
+    return 1
+  }
 
   first_elem_id=${2:-1}
-  local -a found=( "$(up_hierarchy_search "$1" '.autoenv-evn.*')" )
+  local -a found=()
+  while IFS= read -r line; do
+    [[ -n "$line" ]] && found+=("$line")
+  done < <(up_hierarchy_search "$1" '.autoenv-evn.*')
 
-  if [[ -z ${found[*]} ]]
-  then
+  if [[ -z ${found[*]} ]]; then
     conda activate base
     return 0
   fi
 
-  if [[ $(echo "${found[@]}" | wc -l) -gt 1 ]]
-  then
+  if [[ ${#found[@]} -gt 1 ]]; then
     echo There are several autoenv-files found:
-    echo ${found[*]}
+    echo "${found[*]}"
     echo
     echo Note, the autoenv-script tries to activate only the environment
-    echo 'corresponding to the first autoenv-file listed by `ls`.'
+    echo "corresponding to the first autoenv-file listed by \`ls\`."
     echo Please, keep only one '.autoenv-evn.*'
     echo
   fi
 
-  local file="${found[$first_elem_id]}" ENV
-  ENV=${file##*.}
+  local file _env_
+  file="${found[$first_elem_id]}"
+  _env_=${file##*.}
 
-  ! [[ $CONDA_DEFAULT_ENV =~ $ENV ]] && conda activate $ENV
+  [[ $CONDA_DEFAULT_ENV != "$_env_" ]] && conda activate "$_env_"
 }
-
 
 _conda_autoenv_bash() {
   [[ "$PWD" = "$PREV_WORK_DIR" ]] && return
@@ -75,43 +79,48 @@ _conda_autoenv_bash() {
   PREV_WORK_DIR="$PWD"
 }
 
-_conda_autoenv_zsh () {
-  _conda_autoenv "$PWD"
+_conda_autoenv_zsh() {
+  if [[ -o KSH_ARRAYS ]]; then
+    _conda_autoenv "$PWD" 0
+  else
+    _conda_autoenv "$PWD" 1
+  fi
 }
 
-
-mkenv () {
-  if [[ -n $1 ]]
-  then
-    conda activate $1 || { echo "Create environment $1 first"; return; }
+mkenv() {
+  if [[ -n $1 ]]; then
+    conda activate "$1" || {
+      echo "Create environment $1 first"
+      return
+    }
   fi
 
   if [[ -f environment.yml ]]; then
+    local _env_
     echo "Found 'environment.yml'. Creating env from it.."
-    local ENV=$(head -n 1 environment.yml | cut -d ' ' -f2)
-    { command rm -rf .autoenv-evn.*; } 2> /dev/null
-    ( (conda env list | grep -q "^$ENV") \
-      || conda env create -q -f environment.yml ) \
-        && conda activate $ENV \
-        && touch .autoenv-evn.$ENV
+    _env_=$(
+      grep '^name:' environment.yml |
+        head -n1 | sed 's/name:[[:space:]]*//' | tr -d "\"'"
+    )
+    { command rm -rf .autoenv-evn.*; } 2>/dev/null
+    ( (conda env list | grep -q "^$_env_") ||
+      conda env create -q -f environment.yml) &&
+      conda activate "$_env_" &&
+      touch ".autoenv-evn.$_env_"
 
   elif [[ $CONDA_DEFAULT_ENV != base ]]; then
-    { command rm -rf .autoenv-evn.'*'; } 2> /dev/null
-    touch .autoenv-evn.$CONDA_DEFAULT_ENV
+    { command rm -rf .autoenv-evn.'*'; } 2>/dev/null
+    touch ".autoenv-evn.$CONDA_DEFAULT_ENV"
   fi
 }
 
-
-
-if [[ ${HISTFILE##*/} =~ zsh ]]
-then
+if [[ -n $ZSH_VERSION ]]; then
   ## 'chpwd' hook fires when the directory is changed.
   autoload -U add-zsh-hook
   add-zsh-hook chpwd _conda_autoenv_zsh
   ## Run when starting a new shell instance.
   _conda_autoenv_zsh
-elif [[ ${HISTFILE##*/} =~ bash ]]
-then
+elif [[ -n $BASH_VERSION ]]; then
   ## PROMPT_COMMAND contents is executed before each prompt.
   ## However, we want to be able to run `conda deactivate` in a directory
   ## with .autoenv-evn.* file. To this end, we use PREV_WORK_DIR variable
@@ -121,5 +130,6 @@ then
   ## PREV_WORK_DIR with non-existent path
   PREV_WORK_DIR=/-/
   PROMPT_COMMAND="_conda_autoenv_bash; $PROMPT_COMMAND"
+else
+  >&2 echo 'Impl.error: autoenv works only with bash or zsh.'
 fi
-
